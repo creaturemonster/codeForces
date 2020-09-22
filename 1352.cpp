b5569750e3b664951833fae0128a9bb99dc4f2e6
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-21 23:27:46 -0400

    Update 1352.cpp

--- 1352.cpp
+++ 1352.cpp
@@ -11,10 +11,15 @@ int main()
     
     String num = to_string(n);
     int num_zeros=0;
-    for(char c: num){
+    for(int i=0;i<num.length;i++){
+      char c=num.at(i);
         if(c=='0'){
           num_zeroes++;
         }
+      else{
+         int p=(int) c;
+         cout << p*num.length() -i;
+      }
         
       }
     int res= num.length() - num_zeroes;
